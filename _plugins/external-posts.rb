7f50230dd6aa034f9acda8263ef3161a35fdc92c
Michael Vasiljev <gnu.xinm@gmail.com>
2025-11-23 12:43:34 +0300

    fix unmerged changes (#43)

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -25,16 +25,12 @@ module ExternalPosts
     def fetch_from_rss(site, src)
       xml = HTTParty.get(src['rss_url']).body
       return if xml.nil?
-<<<<<<< HEAD
-      feed = Feedjira.parse(xml)
-=======
       begin
         feed = Feedjira.parse(xml)
       rescue StandardError => e
         puts "Error parsing RSS feed from #{src['rss_url']} - #{e.message}"
         return
       end
->>>>>>> v0.14.7
       process_entries(site, src, feed.entries)
     end
 
